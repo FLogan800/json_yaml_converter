dd855bb26d14a2c7ca938216bafa7e69e9821d6a
Logan Ferris <FLogan800@gmail.com>
2026-01-23 11:19:48 -0600

    Add json token types

---
+++ src/json_token_type.h
@@ -0,0 +1,21 @@
+enum token_type
+{
+    // Structural tokens
+    token_left_square_bracket,
+    token_right_square_bracket,
+    token_left_curly_bracket,
+    token_right_curly_bracket,
+    token_colon,
+    token_comma,
+
+    // Literal name tokens
+    token_true,
+    token_false,
+    token_null,
+
+    // Value tokens
+    token_object,
+    token_array,
+    token_number,
+    token_string,
+};
